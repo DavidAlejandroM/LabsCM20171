55716680120868a7ebda4fba37f602eaffed538b
JohanaCt <leidy.caro@udea.edu.co>
2017-03-13 06:40:06 -0500

    S

--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr01_20171/lab2/Login.java
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr01_20171/lab2/Login.java
@@ -1,22 +1,68 @@
 package co.edu.udea.compumovil.gr01_20171.lab2;
 
 import android.app.Activity;
-import android.content.pm.ActivityInfo;
-import android.support.v7.app.AppCompatActivity;
+import android.app.Fragment;
+import android.os.Build;
 import android.os.Bundle;
-import android.view.Window;
+import android.support.annotation.RequiresApi;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login extends Activity {
+import co.edu.udea.compumovil.gr01_20171.lab2.listener.OnFragmentInteractionListener;
 
+
+public class Login extends Fragment implements View.OnClickListener  {
+
+
+    public static final int id = 1 ;
+    Button registro,iniciar;
+    EditText user, pass;
+
+    private OnFragmentInteractionListener mlistener;
+
+    public static Login newInstance(){
+        return new Login();
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.activity_login, container, false);
+    }
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        super.onCreateView(view, savedInstanceState);
+        registro = (Button)view.findViewById(R.id.registro);
+        registro.setOnClickListener(this);
+        iniciar = (Button)view.findViewById(R.id.iniciar);
+        iniciar.setOnClickListener(this);
+        pass =(EditText) view.findViewById(R.id.et_contraseña);
+        user = (EditText) view.findViewById(R.id.et_login_usuario);
+        mlistener = (OnFragmentInteractionListener) getActivity();
+    }
 
-        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        //Para Ocultar la barra de titulo
-        requestWindowFeature(Window.FEATURE_NO_TITLE);
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.registro:
+                mlistener.setFragment(registro.id,null,false);
+                break;
+            case R.id.iniciar:
+                String usu = user.getText().toString();
+                String pas = pass.getText().toString();
 
-        setContentView(R.layout.activity_login);
+                if(pas.isEmpty()){
+                    Toast.makeText(getContext(), R.string.falta_dato, Toast.LENGTH_LONG).show();
+                    return;
+                }
+                if(usu.isEmpty()){
+                    Toast.makeText(getContext(), R.string.falta_dato, Toast.LENGTH_LONG).show();
+                    return;
+                }
+
+        }
     }
 }
