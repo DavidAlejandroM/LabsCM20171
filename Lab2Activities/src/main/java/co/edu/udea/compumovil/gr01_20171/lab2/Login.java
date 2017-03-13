6c41f51b15a76ad55345e6a11ea1d5a9b3d0334a
David Alejandro Marin Alzate <jandro240@gmail.com>
2017-03-12 21:14:36 -0500

    implementando db

--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr01_20171/lab2/Login.java
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr01_20171/lab2/Login.java
@@ -1,9 +1,11 @@
 package co.edu.udea.compumovil.gr01_20171.lab2;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class Login extends Activity {
@@ -18,5 +20,17 @@ public class Login extends Activity {
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
         setContentView(R.layout.activity_login);
+    }
+
+    public void goToNavigation()
+    {
+        Intent intent = new Intent(Login.this,navigation.class);
+        startActivity(intent);
+
+    }
+
+    public void ClickInicioSesion(View view)
+    {
+        goToNavigation();
     }
 }
--- Lab2Activities/src/main/java/co/edu/udea/compumovil/gr01_20171/lab2/db/DbContract.java
+++ Lab2Activities/src/main/java/co/edu/udea/compumovil/gr01_20171/lab2/db/DbContract.java
@@ -1,11 +1,19 @@
 package co.edu.udea.compumovil.gr01_20171.lab2.db;
 
+import android.provider.BaseColumns;
+
 /**
  * Created by Alejandro on 11/03/2017.
  */
 
 public final class DbContract {
     private DbContract() {
+    }
+
+    public static class DbEntry implements BaseColumns
+    {
+        public static final String TN_USUARIOS = "tbl_usuarios";
+
     }
 
 
