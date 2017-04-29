9c0f6c7a64dd4dbc275e7b0660c360b0f3045396
David Alejandro Marin Alzate <jandro240@gmail.com>
2017-04-29 17:12:53 -0500

    ya detecta la informacion del usuario

--- Lab4FCM/src/main/java/co/edu/udea/compumovil/g01_20171/lab4/Main.java
+++ Lab4FCM/src/main/java/co/edu/udea/compumovil/g01_20171/lab4/Main.java
@@ -6,6 +6,7 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
 import android.support.v4.view.GravityCompat;
@@ -15,6 +16,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
@@ -23,11 +25,13 @@ public class Main extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
     private FirebaseAuth auth;
+    private FirebaseUser usuario;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         auth = FirebaseAuth.getInstance();
+        usuario = auth.getCurrentUser();
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
@@ -49,6 +53,7 @@ public class Main extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
     }
 
     @Override
@@ -65,7 +70,18 @@ public class Main extends AppCompatActivity
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
+
+        updateInfoUsuerMenu();
+
         return true;
+    }
+
+    private void updateInfoUsuerMenu() {
+        TextView correoMenu = (TextView) findViewById(R.id.tv_menu_correo);
+        TextView nombreMenu = (TextView) findViewById(R.id.tv_menu_nombre);
+
+        correoMenu.setText(usuario.getEmail());
+        nombreMenu.setText(usuario.getDisplayName());
     }
 
     @Override
@@ -82,6 +98,7 @@ public class Main extends AppCompatActivity
 
         return super.onOptionsItemSelected(item);
     }
+
 
     @SuppressWarnings("StatementWithEmptyBody")
     @Override
@@ -103,19 +120,12 @@ public class Main extends AppCompatActivity
         } else if (id == R.id.nav_cerrar) {
             auth.signOut();
 
-// this listener will be called when there is change in firebase user session
-            FirebaseAuth.AuthStateListener authListener = new FirebaseAuth.AuthStateListener() {
-                @Override
-                public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                    FirebaseUser user = firebaseAuth.getCurrentUser();
-                    if (user == null) {
-                        // user auth state is changed - user is null
-                        // launch login activity
-                        startActivity(new Intent(Main.this, InicioSesion.class));
-                        finish();
-                    }
-                }
-            };
+            // this listener will be called when there is change in firebase user session
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (user == null) {
+                startActivity(new Intent(Main.this,InicioSesion.class));
+                finish();
+            }
         }
 
         if(fragmentTransaction) {
